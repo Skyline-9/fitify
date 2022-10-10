820f61ffd66e1fcfefc3fc7b78ff90e7bea89159
Skyline-9 <luo.richard@gmail.com>
2022-10-10 12:21:14 -0400

    🐛Fix dependency issues with react-native-ui

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {Colors, LoaderScreen} from "react-native-ui-lib";
+import {NativeScreenNavigationContainer} from "react-native-screens";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NativeScreenNavigationContainer>
+
+      </NativeScreenNavigationContainer>
   );
 }
 
--- README.md
+++ README.md
@@ -8,3 +8,7 @@ Run `yarn expo start` to start development server
 ## Libraries
 
 Library documentation: https://wix.github.io/react-native-ui-lib/docs/getting-started/setup
+
+## References
+
+I recommend looking at https://github.com/wix/react-native-ui-lib/tree/master/demo
