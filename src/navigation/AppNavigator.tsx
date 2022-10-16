46b64a11e53264b26cb8ea46ef949eafcddad44b
Skyline-9 <luo.richard@gmail.com>
2022-10-15 21:12:14 -0400

    ✨Added working firebase auth

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -9,17 +9,15 @@ import {AuthContext} from "../provider/AuthProvider";
 // @ts-ignore
 import {API_KEY, APP_ID, AUTH_DOMAIN, DATABASE_URL, MESSAGING_SENDER_ID, PROJECT_ID, STORAGE_BUCKET} from "@env"
 
-// Main
-import Home from "../screens/Home";
-
 // Auth screens
-// import Login from "../screens/auth/Login";
-// import Register from "../screens/auth/Register";
-// import ForgetPassword from "../screens/auth/ForgetPassword";
-//
-// import Loading from "../screens/utils/Loading";
-// import HomeScreen from "../screens/Home";
-// import ProfileScreen from "../screens/ProfileScreen";
+import Login from "../screens/auth/Login";
+import Register from "../screens/auth/Register";
+import ForgetPassword from "../screens/auth/ForgetPassword";
+
+//Main
+import Loading from "../screens/utils/Loading";
+import HomeScreen from "../screens/Home";
+import ProfileScreen from "../screens/ProfileScreen";
 
 // Better put your these secret keys in .env file
 const firebaseConfig = {
@@ -31,55 +29,55 @@ const firebaseConfig = {
     messagingSenderId: MESSAGING_SENDER_ID,
     appId: APP_ID,
 };
-//
-// if (getApps().length === 0) {
-//     const app = initializeApp(firebaseConfig);
-//     initializeFirestore(app, {experimentalForceLongPolling: true, merge: true})
-// }
-//
-// const AuthStack = createNativeStackNavigator();
-//
-// const Auth = () => {
-//     return (
-//         <AuthStack.Navigator
-//             screenOptions={{
-//                 headerShown: false,
-//             }}
-//         >
-//             <AuthStack.Screen name="Login" component={Login}/>
-//             <AuthStack.Screen name="Register" component={Register}/>
-//             <AuthStack.Screen name="ForgetPassword" component={ForgetPassword}/>
-//         </AuthStack.Navigator>
-//     );
-// };
-//
-// const MainStack = createNativeStackNavigator();
-//
-// const Main = () => {
-//     return (
-//         <MainStack.Navigator
-//             // screenOptions={{
-//             //     headerShown: false,
-//             // }}
-//         >
-//             <MainStack.Screen
-//                 name="Home"
-//                 component={HomeScreen}
-//                 options={{title: 'Welcome'}}
-//             />
-//             <MainStack.Screen name="Profile" component={ProfileScreen}/>
-//         </MainStack.Navigator>
-//     );
-// };
-//
-// export default () => {
-//     const auth = useContext(AuthContext);
-//     const user = auth.user;
-//     return (
-//         <NavigationContainer>
-//             {user == null && <Loading/>}
-//             {user == false && <Auth/>}
-//             {user == true && <Main/>}
-//         </NavigationContainer>
-//     );
-// };
+
+if (getApps().length === 0) {
+    const app = initializeApp(firebaseConfig);
+    initializeFirestore(app, {experimentalForceLongPolling: true})
+}
+
+const AuthStack = createNativeStackNavigator();
+
+const Auth = () => {
+    return (
+        <AuthStack.Navigator
+            screenOptions={{
+                headerShown: false,
+            }}
+        >
+            <AuthStack.Screen name="Login" component={Login}/>
+            <AuthStack.Screen name="Register" component={Register}/>
+            <AuthStack.Screen name="ForgetPassword" component={ForgetPassword}/>
+        </AuthStack.Navigator>
+    );
+};
+
+const MainStack = createNativeStackNavigator();
+
+const Main = () => {
+    return (
+        <MainStack.Navigator
+            // screenOptions={{
+            //     headerShown: false,
+            // }}
+        >
+            <MainStack.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{title: 'Welcome'}}
+            />
+            <MainStack.Screen name="Profile" component={ProfileScreen}/>
+        </MainStack.Navigator>
+    );
+};
+
+export default () => {
+    const auth = useContext(AuthContext);
+    const user = auth.user;
+    return (
+        <NavigationContainer>
+            {user == null && <Loading/>}
+            {user == false && <Auth/>}
+            {user == true && <Main/>}
+        </NavigationContainer>
+    );
+};
