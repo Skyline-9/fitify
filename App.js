82cdce0467ebe53121dd20392e68d36965c092b3
Skyline-9 <luo.richard@gmail.com>
2022-12-01 16:15:07 -0500

    :sparkles: Add working post screen

--- App.js
+++ App.js
@@ -16,7 +16,8 @@ if (!global.atob) {
 export default function App(props) {
     // Ignore firebase v9 AsyncStorage warning
     React.useEffect(() => {
-        LogBox.ignoreLogs(["AsyncStorage has been extracted from react-native core and will be removed in a future release. It can now be installed and imported from '@react-native-async-storage/async-storage' instead of 'react-native'. See https://github.com/react-native-async-storage/async-storage",]);
+        // LogBox.ignoreLogs(["AsyncStorage has been extracted from react-native core and will be removed in a future release. It can now be installed and imported from '@react-native-async-storage/async-storage' instead of 'react-native'. See https://github.com/react-native-async-storage/async-storage",]);
+        LogBox.ignoreAllLogs();
     }, []);
 
     return (<DBProvider>
